17bf04f25d8e17c0d0ebaf4210bebfb135bf82a7
stevesscb <stevesscb@gmail.com>
2022-02-18 15:35:35 +0800

    added routes/api/view for sign up page

---
+++ src/controllers/pages/user/signup.js
@@ -0,0 +1,5 @@
+const controllersPagesUserSignup = async (req, res) => {
+  res.render('user/signup')
+}
+
+export default controllersPagesUserSignup
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 import { Router } from 'express'
 
+// import authenticateUser from './_middlewares/authenticate-user.js'
+
 const router = Router()
+
+// API | USER
+router.post('/api/user/signup', (await import('./controllers/api/user/signup.js')).default)
+
+// PAGES | USER
+router.get('/user/signup', (await import('./controllers/pages/user/signup.js')).default)
 
 // STATIC
 router.get('/', (await import('./controllers/static/home.js')).default)
