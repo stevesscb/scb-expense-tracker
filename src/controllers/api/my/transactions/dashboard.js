114ec3c575f3ebd02443bcfa9d0e1a0ea04f431a
Denis Cheung <d.s.h.cheung@gmail.com>
2022-03-02 14:50:07 +0800

    changed date query

--- src/controllers/api/my/transactions/dashboard.js
+++ src/controllers/api/my/transactions/dashboard.js
@@ -7,18 +7,28 @@ const controllersApiMyDashboard = async (req, res) => {
     const userId = req.session.user.id
     const orderBy = req.query.orderBy || 'id'
     const sortBy = req.query.sortBy || 'asc'
-    const date = req.query.date || '30'
+    const date = req.query.date || 'All'
+
+    const where = { userId, date: {} }
+
+    switch (date) {
+      case 'Today':
+        where.date.gte = new Date(moment().startOf('day'))
+        break
+      case '2Week':
+        where.date.gte = new Date(moment().startOf('week').subtract(7))
+        break
+      case 'Month':
+        where.date.gte = new Date(moment().startOf('month'))
+        break
+      default:
+        break
+    }
 
     const foundTransactions = await prisma.transaction.findMany({
-      where: {
-        userId,
-        date: {
-          gte: new Date(moment().subtract(date, 'days'))
-        }
-      },
+      where,
       orderBy: {
         [orderBy]: sortBy
-        // id: asc
       },
       include: {
         category: true
